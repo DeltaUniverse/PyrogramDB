269d5deee5181b288837052a29d96fc111d6ef29
tomyprs <tomiprs25@gmail.com>
2024-06-16 12:49:07 +0700

    bump version
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- PyroDB/__init__.py
+++ PyroDB/__init__.py
@@ -1,4 +1,4 @@
-from PyroDB.__main__ import PyroDB
+from PyroDB.__main__ import PyrogramDB
 
-__version__ = "V0.1.0"
+__version__ = "V0.3.12"
 __author__ = "Jigarvarma2005 (Jigarverma2002@gmail.com)"
